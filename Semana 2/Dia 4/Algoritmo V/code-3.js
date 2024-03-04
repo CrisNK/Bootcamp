341d08655cc2a9d80351e921d0581e78e78d27c1
Cristobal <cristobalalarcon22@gmail.com>
2024-03-04 16:04:34 -0300

    codigo 3 - terminado

---
+++ Semana 2/Dia 4/Algoritmo V/code-3.js
@@ -0,0 +1,4 @@
+//Configura un array para que el resultado sean los valores en el orden contrario. Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1].
+let array = [1, 2, 3];
+let newArray = array.reverse();
+console.log(newArray);
