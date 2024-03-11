5ae7334ff8aaecfdb01d79f9b68fab3faf153181
Cristobal <cristobalalarcon22@gmail.com>
2024-03-10 21:08:24 -0300

    avance en pacman - falta poder rotar la cara del pacman

--- Semana 3/Dia 3 - Pacman/script.js
+++ Semana 3/Dia 3 - Pacman/script.js
@@ -1,14 +1,27 @@
 var world = [
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 1, 1, 2, 1, 1, 1, 1, 1, 2],
-    [2, 1, 1, 2, 1, 2, 2, 2, 1, 2],
-    [2, 1, 1, 2, 1, 2, 1, 2, 1, 2],
-    [2, 1, 1, 2, 1, 2, 1, 2, 1, 2],
-    [2, 1, 1, 2, 2, 2, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 2, 1, 2],
-    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 3, 2, 2, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 1, 2, 2, 3, 2],
+    [2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
+    [2, 1, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 1, 2],
+    [2, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 2],
+    [2, 2, 2, 2, 1, 1, 1, 2, 2, 0, 2, 2, 1, 1, 1, 2, 2, 2, 2],
+    [0, 1, 1, 1, 1, 2, 1, 2, 0, 0, 0, 2, 1, 2, 1, 1, 1, 1, 0],
+    [2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2],
+    [2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
+    [2, 1, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 1, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 3, 2, 2, 2, 1, 2, 2, 1, 2, 1, 2, 2, 1, 2, 2, 2, 3, 2],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 ];
+
+var pacman = {
+    x: 1,
+    y: 1
+};
+
+var score = 0;
 
 function displayWorld() {
     var output = '';
@@ -16,6 +29,9 @@ function displayWorld() {
     for (var i = 0; i < world.length; i++) {
         output += "\n<div class='row'>";
         for (var j = 0; j < world[i].length; j++) {
+            if (world[i][j] == 3) {
+                output += "\n\t<div class='cherry'></div>"
+            }
             if (world[i][j] == 2) {
                 output += "\n\t<div class='brick'></div>"
             }
@@ -28,7 +44,47 @@ function displayWorld() {
         }
         output += "\n</div>";
     }
-    console.log(output);
+    // console.log(output);
     document.getElementById('world').innerHTML = output;
 }
+function displayPacman() {
+    document.getElementById('pacman').style.top = pacman.y * 20 + 'px';
+    document.getElementById('pacman').style.left = pacman.x * 20 + 'px';
+}
+function displayScore() {
+    document.getElementById('score').innerHTML = score;
+}
 displayWorld();
+displayPacman();
+document.onkeydown = function (e) {
+
+    if (e.keyCode == 37 && (world[pacman.y][pacman.x - 1] != 2)) { // flecha izquierda
+        pacman.x--;
+        document.getElementById('pacman').style.transform = 'scaleX(1)';
+    }
+    if (e.keyCode == 39 && (world[pacman.y][pacman.x + 1] != 2)) { // flecha derecha
+        pacman.x++;
+        document.getElementById('pacman').style.transform = 'scaleX(-1)';
+    }
+    if (e.keyCode == 38 && (world[pacman.y - 1][pacman.x] != 2)) { // flecha arriba
+        pacman.y--;
+        document.getElementById('pacman').style.transform = 'scaleY(-1)';
+        document.getElementById('pacman').style.transformOrigin = 'center';
+    }
+    if (e.keyCode == 40 && (world[pacman.y + 1][pacman.x] != 2)) { // flecha abajo
+        pacman.y++;
+        document.getElementById('pacman').style.transform = 'scaleY(-1)';
+    }
+    if (world[pacman.y][pacman.x] == 1 || world[pacman.y][pacman.x] == 3) {
+        if (world[pacman.y][pacman.x] == 3) {
+            score += 50;
+        } else {
+            score+=10;
+        }
+        world[pacman.y][pacman.x] = 0;
+        displayWorld();
+        displayScore();
+    }
+    displayPacman();
+    console.log(e.keyCode);
+}
